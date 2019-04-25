2705a31f28ed924fd4b344cba43c90d2b1e3bd22
I <danibachar89@gmail.com>
2019-04-25 14:58:37 +0300

    wip

--- Project_1/python/spv_wallet/app.py
+++ Project_1/python/spv_wallet/app.py
@@ -13,6 +13,8 @@ from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
+spv = BlockchainSPV()
+
 @app.route('/')
 def index():
 	return render_template('./index.html')
@@ -26,6 +28,22 @@ def new_wallet():
 		'private_key': binascii.hexlify(private_key.exportKey(format='DER')).decode('ascii'),
 		'public_key': binascii.hexlify(public_key.exportKey(format='DER')).decode('ascii')
 	}
+
+	return jsonify(response), 200
+
+@app.route('/validate/transaction', methods=['POST'])
+def validate_transaction():
+
+	sender_address = request.form['sender_address']
+	sender_private_key = request.form['sender_private_key']
+	recipient_address = request.form['recipient_address']
+	value = request.form['amount']
+
+	transaction = Transaction(sender_address, sender_private_key, recipient_address, value)
+
+	response = {'transaction': transaction.to_dict(), 'signature': transaction.sign_transaction()}
+
+
 
 	return jsonify(response), 200
 
