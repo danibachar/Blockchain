f0f43041663674028c67ce1ac7ebce9bf7e610ca
I <danibachar89@gmail.com>
2019-07-22 16:51:35 +0300

    remove bibicoin

--- Final_Project/election/migrations/2_deploy_contracts.js
+++ Final_Project/election/migrations/2_deploy_contracts.js
@@ -1,17 +1,5 @@
 var Election = artifacts.require("./Election.sol");
-var BibiCoin = artifacts.require("./BibiCoin.sol");
 
-// module.exports = function(deployer) {
-//   deployer.deploy(Election)
-//     .then(function(){
-//       return deployer
-//         .deploy(BibiCoin, Election.address, 626)
-//     });
-// };
-//
-//
-//
-//
 module.exports = function(deployer) {
   deployer.deploy(Election);
 };
